3551133d08ae3f984dfbb65b30c693f961dbdbba
AdamKj <ak.98@live.se>
2021-01-07 19:38:34 +0100

    new navbar

--- mammashemsida/src/App.js
+++ mammashemsida/src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import {Home} from './components/Home/Home';
-import {Navbar} from './components/Navbar/Navbar';
+import {NavBar} from './components/Navbar/Navbar';
 import {Services} from './components/Services/Services';
 import {About} from './components/About HK/About HK';
 import {CustomerReviews} from './components/Customer Reviews/CustomerReviews';
@@ -10,7 +10,7 @@ import {CustomerReviews} from './components/Customer Reviews/CustomerReviews';
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <NavBar />
       <Router>
         <Switch>
           <Route exact path="/" component={Home} />
--- mammashemsida/src/components/Navbar/Navbar.js
+++ mammashemsida/src/components/Navbar/Navbar.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import './Navbar.css';
+import Navbar from 'react-bootstrap/Navbar'
+import {Nav, Form, FormControl, Button} from "react-bootstrap"
 
-export const Navbar = () => {
+export const NavBar = () => {
     return (
       <div className="wrapper">
-        <ul>
-          <li>
-            <a href="/">Hem</a>
-          </li>
-          <li >
-            <a href="/tjanster">Tjänster</a>
-          </li>
-          <li>
-             <a href="/om hk-redovisning">Om HK Redovisning</a>
-          </li>
-          <li>
-            <a href="/referenser-omdomen">Referenser & Omdömen</a>
-          </li>
-          <li>
-            *fixa en sökruta här*
-          </li>
-        </ul>
+        <Navbar bg="light" expand="lg">
+  <Navbar.Brand>HK Redovisning</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="/">Hem</Nav.Link>
+      <Nav.Link href="tjanster">Tjänster</Nav.Link>
+      <Nav.Link href="om hk-redovisning">Om HK Redovisning</Nav.Link>
+      <Nav.Link href="referenser-omdomen">Referenser & Omdömen</Nav.Link>
+    </Nav>
+            <Form inline>
+              <FormControl type="text" placeholder="Sök" className="mr-sm-2" />
+                <Button variant="outline-success">Sök</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Navbar>
       </div>
     );
-  };
+  }
